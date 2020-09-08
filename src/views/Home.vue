97e9520d6e1feb8cc9c8ccb17c4ba7397d7765c7
miny-simp <rzl_m1022@163.com>
2020-09-08 17:30:14 +0800

    内容修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <el-header>
       <HeaderBox />
     </el-header>
-    <el-container>
-      <el-aside width="200px">
+    <el-container style="height: calc(100% - 70px);">
+      <el-aside width="220px">
         <NavMenu />
       </el-aside>
       <el-main>
@@ -31,27 +31,31 @@ export default {
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.el-header {
-  background-color: #454d58;
-  /* background: url('~@/assets/images/header_bg.png') center center no-repeat; */
-  height: 70px;
-  color: #fff;
-}
-
-/* .el-aside {
-  background-color: #454d58;
-  color: #fff;
-} */
-
-.el-main {
-  background-color: #E9EEF3;
-  color: #333;
-}
-
-.el-container.is-vertical {
+<style lang="less">
+.is-vertical {
   height: 100%;
+  .el-header {
+    background-color: #454d58;
+    /* background: url('~@/assets/images/header_bg.png') center center no-repeat; */
+    height: 70px;
+    border-bottom: 1px solid #9E9E9E;
+    color: #fff;
+  }
+  .el-aside {
+    scrollbar-width: none; /* firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+  }
+  .el-aside::-webkit-scrollbar {
+    display: none; /* Chrome Safari */
+  }
+  .el-main {
+    background-color: #fff;
+    margin: 10px;
+    padding: 10px;
+    color: #333;
+  }
+}
+.el-form-item--mini.el-form-item, .el-form-item--small.el-form-item {
+  margin-bottom: 10px;
 }
 </style>
